457a4ba814c373bea2c95f6cd1a5df86fdf3db1b
Ruslan <ruslan.hilinsky@yandex.ru>
2023-11-24 02:42:58 +0300

    Лабораторная работа № 2

--- Control Task 02/SrtuctType/SrtuctType/Program.cs
+++ Control Task 02/SrtuctType/SrtuctType/Program.cs
@@ -22,24 +22,22 @@ namespace StructType
     {
         static void Main(string[] args)
         {
-            Distance a, b, c;
-
             Console.WriteLine("Please enter the first feet value: ");
             int feet1 = int.Parse(Console.ReadLine());
             Console.WriteLine("Please enter the first inches value: ");
             int inches1 = int.Parse(Console.ReadLine());
 
-            a = new Distance(feet1, inches1);
+            Distance a = new Distance(feet1, inches1);
 
-            Console.Write("Please enter the second feet value: ");
+            Console.WriteLine("Please enter the second feet value: ");
             int feet2 = int.Parse(Console.ReadLine());
-            Console.Write("Please enter the second inches value: ");
+            Console.WriteLine("Please enter the second inches value: ");
             int inches2 = int.Parse(Console.ReadLine());
 
-            b = new Distance(feet2, inches2);
+            Distance b = new Distance(feet2, inches2);
 
             int totalInches = a.feet * 12 + a.inches + b.feet * 12 + b.inches;
-            c = new Distance(totalInches / 12, totalInches % 12);
+            Distance c = new Distance(totalInches / 12, totalInches % 12);
 
             Console.WriteLine("Distance sum is");
             Console.WriteLine($"{a.feet}'-{a.inches}\" + {b.feet}'-{b.inches}\" = {c.feet}'-{c.inches}\"");
